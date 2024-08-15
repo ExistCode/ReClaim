3e48c0a8ae6bcaeb632dae79a9da0f168ca688bb
bonifacioronald <bonifacioronald99@gmail.com>
2024-08-15 16:25:48 +0800

    Added assets, fonts, images

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'ReClaim',
+      theme: ThemeData(fontFamily: 'Inter'),
       home: Navigation(),
     );
   }
